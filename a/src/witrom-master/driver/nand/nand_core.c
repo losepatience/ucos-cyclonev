b47920ab9f41a6210b204c5b2834c0a43788e6db
John Lee <furious_tauren@163.com>
2014-06-26 00:43:40 +0800

    kdk

--- a/src/witrom-master/driver/nand/nand_core.c
+++ a/src/witrom-master/driver/nand/nand_core.c
@@ -19,172 +19,151 @@
  * MA 02111-1307 USA
  */
 
-#include <io.h>
-#include <stdio.h>
-#include <delay.h>
+#include <asm/arch/regs.h>
+#include <asm/io.h>
 #include <flash/nand.h>
 
-// #define CONFIG_NAND_DEBUG
-#define NAND_TIMEOUT         (1 << 14)
-#define IS_LARGE_PAGE(nand)  (nand->write_size >= KB(1))
-
-static const struct nand_desc g_nand_chip_desc[] = {
-    NAND_CHIP_DESC("NAND 16MB 1.8V 8-bit",   0x33, 512, 16, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 16MB 3.3V 8-bit",   0x73, 512, 16, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 16MB 1.8V 16-bit",  0x43, 512, 16, 0x4000, NAND_BUSWIDTH_16),
-    NAND_CHIP_DESC("NAND 16MB 3.3V 16-bit",  0x53, 512, 16, 0x4000, NAND_BUSWIDTH_16),
-
-    NAND_CHIP_DESC("NAND 32MB 1.8V 8-bit",   0x35, 512, 32, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 32MB 3.3V 8-bit",   0x75, 512, 32, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 32MB 1.8V 16-bit",  0x45, 512, 32, 0x4000, NAND_BUSWIDTH_16),
-    NAND_CHIP_DESC("NAND 32MB 3.3V 16-bit",  0x55, 512, 32, 0x4000, NAND_BUSWIDTH_16),
-
-    NAND_CHIP_DESC("NAND 64MB 1.8V 8-bit",   0x36, 512, 64, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 64MB 3.3V 8-bit",   0x76, 512, 64, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 64MB 1.8V 16-bit",  0x46, 512, 64, 0x4000, NAND_BUSWIDTH_16),
-    NAND_CHIP_DESC("NAND 64MB 3.3V 16-bit",  0x56, 512, 64, 0x4000, NAND_BUSWIDTH_16),
-
-    NAND_CHIP_DESC("NAND 128MB 1.8V 8-bit",  0x78, 512, 128, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 128MB 1.8V 8-bit",  0x39, 512, 128, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 128MB 3.3V 8-bit",  0x79, 512, 128, 0x4000, 0),
-    NAND_CHIP_DESC("NAND 128MB 1.8V 16-bit", 0x72, 512, 128, 0x4000, NAND_BUSWIDTH_16),
-    NAND_CHIP_DESC("NAND 128MB 1.8V 16-bit", 0x49, 512, 128, 0x4000, NAND_BUSWIDTH_16),
-    NAND_CHIP_DESC("NAND 128MB 3.3V 16-bit", 0x74, 512, 128, 0x4000, NAND_BUSWIDTH_16),
-    NAND_CHIP_DESC("NAND 128MB 3.3V 16-bit", 0x59, 512, 128, 0x4000, NAND_BUSWIDTH_16),
-
-    NAND_CHIP_DESC("NAND 256MB 3.3V 8-bit",   0x71, 512, 256, 0x4000, 0),
-
-    NAND_CHIP_DESC("NAND 64MB 1.8V 8-bit",   0xA2, 0,  64, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 64MB 3.3V 8-bit",   0xF2, 0,  64, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 64MB 1.8V 16-bit",  0xB2, 0,  64, 0, LP_OPTIONS16),
-    NAND_CHIP_DESC("NAND 64MB 3.3V 16-bit",  0xC2, 0,  64, 0, LP_OPTIONS16),
-
-    NAND_CHIP_DESC("NAND 128MB 1.8V 8-bit",  0xA1, 0, 128, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 128MB 3.3V 8-bit",  0xF1, 0, 128, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 128MB 1.8V 16-bit", 0xB1, 0, 128, 0, LP_OPTIONS16),
-    NAND_CHIP_DESC("NAND 128MB 3.3V 16-bit", 0xC1, 0, 128, 0, LP_OPTIONS16),
-
-    NAND_CHIP_DESC("NAND 256MB 1.8V 8-bit",  0xAA, 0, 256, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 256MB 3.3V 8-bit",  0xDA, 0, 256, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 256MB 1.8V 16-bit", 0xBA, 0, 256, 0, LP_OPTIONS16),
-    NAND_CHIP_DESC("NAND 256MB 3.3V 16-bit", 0xCA, 0, 256, 0, LP_OPTIONS16),
-
-    NAND_CHIP_DESC("NAND 512MB 1.8V 8-bit",  0xAC, 0, 512, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 512MB 3.3V 8-bit",  0xDC, 0, 512, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 512MB 1.8V 16-bit", 0xBC, 0, 512, 0, LP_OPTIONS16),
-    NAND_CHIP_DESC("NAND 512MB 3.3V 16-bit", 0xCC, 0, 512, 0, LP_OPTIONS16),
-
-    NAND_CHIP_DESC("NAND 1GiB 1.8V 8-bit",   0xA3, 0, 1024, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 1GiB 3.3V 8-bit",   0xD3, 0, 1024, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 1GiB 1.8V 16-bit",  0xB3, 0, 1024, 0, LP_OPTIONS16),
-    NAND_CHIP_DESC("NAND 1GiB 3.3V 16-bit",  0xC3, 0, 1024, 0, LP_OPTIONS16),
-
-    NAND_CHIP_DESC("NAND 2GiB 1.8V 8-bit",   0xA5, 0, 2048, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 2GiB 3.3V 8-bit",   0xD5, 0, 2048, 0, LP_OPTIONS),
-    NAND_CHIP_DESC("NAND 2GiB 1.8V 16-bit",  0xB5, 0, 2048, 0, LP_OPTIONS16),
-    NAND_CHIP_DESC("NAND 2GiB 3.3V 16-bit",  0xC5, 0, 2048, 0, LP_OPTIONS16),
-};
-
-static void nand_cmd_ctrl(struct nand_chip *nand, __u8 arg, __u32 ctrl)
+static int omap3_nand_ready(struct nand_chip *chip)
 {
-	if (ctrl & NAND_CLE)
-		writeb(nand->cmmd_port, arg);
-	else // if (ctrl & NAND_ALE)
-		writeb(nand->addr_port, arg);
+	void *gpmc = (void *)GPMC_BASE;
+
+	return readl(gpmc + GPMC_STATUS) & (1 << 8);
 }
 
-static int nand_wait_ready(struct nand_chip *nand)
+static void omap3_cmd_ctrl(struct nand_chip *chip, u32 cmd, u32 ctrl)
 {
-	int i;
+	void *base = (void *)GPMC_BASE + GPMC_CS0 + (0x30 * cs);
 
-	if (nand->nand_ready) {
-		for (i = 0; i < NAND_TIMEOUT; i++) {
-			if (nand->nand_ready(nand))
-				return i;
-		}
-
-#ifdef CONFIG_NAND_DEBUG
-		printf("Nand Timeout!\n");
-#endif
-		return -1;
+	switch (ctrl) {
+	case NAND_CTRL_CLE:
+		chip->IO_ADDR_W = base + GPMC_CS_NAND_CMD;
+		break;
+	case NAND_CTRL_ALE:
+		chip->IO_ADDR_W = base + GPMC_CS_NAND_ADR;
+		break;
+	case NAND_NCE:
+		chip->IO_ADDR_W = base + GPMC_CS_NAND_DAT;
+		break;
 	}
 
-	udelay(0x500);
+	writeb(cmd, chip->IO_ADDR_W);
+}
+
+
+
+
+
+
+
+#define is_large_page(nand)	((nand)->write_size >= 1024)
+
+static const struct nand_desc chip_desc[] = {
+	{ "16MB 1.8V 08-bit",	0x33, 512, 16, 0x4000, 0 },
+	{ "16MB 3.3V 08-bit",	0x73, 512, 16, 0x4000, 0 },
+	{ "16MB 1.8V 16-bit",	0x43, 512, 16, 0x4000, NAND_BUSWIDTH_16 },
+	{ "16MB 3.3V 16-bit",	0x53, 512, 16, 0x4000, NAND_BUSWIDTH_16 },
+
+	{ "32MB 1.8V 08-bit",	0x35, 512, 32, 0x4000, 0 },
+	{ "32MB 3.3V 08-bit",	0x75, 512, 32, 0x4000, 0 },
+	{ "32MB 1.8V 16-bit",	0x45, 512, 32, 0x4000, NAND_BUSWIDTH_16 },
+	{ "32MB 3.3V 16-bit",	0x55, 512, 32, 0x4000, NAND_BUSWIDTH_16 },
+
+	{ "64MB 1.8V 08-bit", 	0x36, 512, 64, 0x4000, 0 },
+	{ "64MB 3.3V 08-bit", 	0x76, 512, 64, 0x4000, 0 },
+	{ "64MB 1.8V 16-bit",	0x46, 512, 64, 0x4000, NAND_BUSWIDTH_16 },
+	{ "64MB 3.3V 16-bit",	0x56, 512, 64, 0x4000, NAND_BUSWIDTH_16 },
+
+	{ "128MB 1.8V 08-bit",	0x78, 512, 128, 0x4000, 0 },
+	{ "128MB 1.8V 08-bit",	0x39, 512, 128, 0x4000, 0 },
+	{ "128MB 3.3V 08-bit",	0x79, 512, 128, 0x4000, 0 },
+	{ "128MB 1.8V 16-bit",	0x72, 512, 128, 0x4000, NAND_BUSWIDTH_16 },
+	{ "128MB 1.8V 16-bit",	0x49, 512, 128, 0x4000, NAND_BUSWIDTH_16 },
+	{ "128MB 3.3V 16-bit",	0x74, 512, 128, 0x4000, NAND_BUSWIDTH_16 },
+	{ "128MB 3.3V 16-bit",	0x59, 512, 128, 0x4000, NAND_BUSWIDTH_16 },
+
+	{ "256MB 3.3V 08-bit",	0x71, 512, 256, 0x4000, 0 },
+
+	{ "64MB 1.8V 08-bit",	0xA2, 0,  64, 0, LP_OPTIONS },
+	{ "64MB 3.3V 08-bit",	0xF2, 0,  64, 0, LP_OPTIONS },
+	{ "64MB 1.8V 16-bit",	0xB2, 0,  64, 0, LP_OPTIONS16 },
+	{ "64MB 3.3V 16-bit",	0xC2, 0,  64, 0, LP_OPTIONS16 },
+
+	{ "128MB 1.8V 08-bit",	0xA1, 0, 128, 0, LP_OPTIONS },
+	{ "128MB 3.3V 08-bit",	0xF1, 0, 128, 0, LP_OPTIONS },
+	{ "128MB 1.8V 16-bit",	0xB1, 0, 128, 0, LP_OPTIONS16 },
+	{ "128MB 3.3V 16-bit",	0xC1, 0, 128, 0, LP_OPTIONS16 },
+
+	{ "256MB 1.8V 08-bit",	0xAA, 0, 256, 0, LP_OPTIONS },
+	{ "256MB 3.3V 08-bit",	0xDA, 0, 256, 0, LP_OPTIONS },
+	{ "256MB 1.8V 16-bit",	0xBA, 0, 256, 0, LP_OPTIONS16 },
+	{ "256MB 3.3V 16-bit",	0xCA, 0, 256, 0, LP_OPTIONS16 },
+
+	{ "512MB 1.8V 08-bit",	0xAC, 0, 512, 0, LP_OPTIONS },
+	{ "512MB 3.3V 08-bit",	0xDC, 0, 512, 0, LP_OPTIONS },
+	{ "512MB 1.8V 16-bit",	0xBC, 0, 512, 0, LP_OPTIONS16 },
+	{ "512MB 3.3V 16-bit",	0xCC, 0, 512, 0, LP_OPTIONS16 },
+
+	{ "1GiB 1.8V 08-bit",	0xA3, 0, 1024, 0, LP_OPTIONS },
+	{ "1GiB 3.3V 08-bit",	0xD3, 0, 1024, 0, LP_OPTIONS },
+	{ "1GiB 1.8V 16-bit",	0xB3, 0, 1024, 0, LP_OPTIONS16 },
+	{ "1GiB 3.3V 16-bit",	0xC3, 0, 1024, 0, LP_OPTIONS16 },
+
+	{ "2GiB 1.8V 08-bit",	0xA5, 0, 2048, 0, LP_OPTIONS },
+	{ "2GiB 3.3V 08-bit",	0xD5, 0, 2048, 0, LP_OPTIONS },
+	{ "2GiB 1.8V 16-bit",	0xB5, 0, 2048, 0, LP_OPTIONS16 },
+	{ "2GiB 3.3V 16-bit",	0xC5, 0, 2048, 0, LP_OPTIONS16 },
+	{ 0 };
+};
+
+static int nand_wait_ready(struct nand_chip *chip)
+{
+	int ready;
+	int retries = 100;
+
+	do {
+		ready = chip->nand_ready(chip);
+		if (ready)
+			return 1;
+
+		udelay(0x500);
+
+	} while (retries--);
 
 	return 0;
 }
 
-static void nand_command(struct nand_chip *nand,
-						__u32 cmd, int row, int col)
+static void config_nand(struct nand_chip *chip, const struct nand_desc *desc)
 {
-	nand_cmd_ctrl(nand, cmd, NAND_CLE);
+	u8 ext_id;
 
-	if (col != -1) {
-		nand_cmd_ctrl(nand, col & 0xff, NAND_ALE);
+	chip->chip_size  = desc->size & 0xFFF00000;
+	chip->write_size = desc->size & 0x000FFFFF;
 
-		if (IS_LARGE_PAGE(nand))
-			nand_cmd_ctrl(nand, (col >> 8) & 0xff, NAND_ALE);
+	if (!chip->write_size) {
+		ext_id = readb(chip->IO_ADDR_R);
+		ext_id = readb(chip->IO_ADDR_R);
+
+		chip->write_size = KB(1) << (ext_id & 0x3);
 	}
 
-	if (row != -1) {
-		nand_cmd_ctrl(nand, row & 0xff, NAND_ALE);
-		nand_cmd_ctrl(nand, (row >> 8) & 0xff, NAND_ALE);
-
-		if (nand->chip_size > (1 << 27))
-			nand_cmd_ctrl(nand, (row >> 16) & 0xff, NAND_ALE);
-	}
-
-	switch (cmd) {
-	case NAND_CMMD_READ0:
-		if (IS_LARGE_PAGE(nand))
-			nand_cmd_ctrl(nand, NAND_CMMD_READSTART, NAND_CLE);
-
-	default:
-		break;
-	}
-
-	nand_wait_ready(nand); // fixme: not need for READID
-}
-
-static inline void config_nand(struct nand_chip *nand,
-						const struct nand_desc *desc)
-{
-	__u8 ext_id;
-
-	nand->chip_size  = desc->size & 0xFFF00000;
-	nand->write_size = desc->size & 0x000FFFFF;
-
-	if (!nand->write_size) {
-		ext_id = readb(nand->data_port);
-		ext_id = readb(nand->data_port);
-
-		nand->write_size = KB(1) << (ext_id & 0x3);
-	}
-
-#ifdef CONFIG_NAND_DEBUG
-	printf("NAND page size = 0x%x, chip size = 0x%x\n",
-		nand->write_size, nand->chip_size);
-#endif
-
-	if (!nand->read_buff) {
+	if (!chip->read_buf) {
 		if (desc->flags & NAND_BUSWIDTH_16)
-			nand->read_buff = nand_read_buff16;
+			chip->read_buf = nand_read_buf16;
 		else
-			nand->read_buff = nand_read_buff8;
+			chip->read_buf = nand_read_buf8;
 	}
 }
 
 int nand_probe(struct nand_chip *nand)
 {
 	int i;
-	__u8 dev_id, ven_id;
+	u8 dev_id, ven_id;
 
 	nand_command(nand, NAND_CMMD_RESET, -1, -1);
-
 	nand_command(nand, NAND_CMMD_READID, -1, 0);
+
 	ven_id = readb(nand->data_port);
 	dev_id = readb(nand->data_port);
-	printf("NAND ID = 0x%x%x\n", ven_id, dev_id);
 
 	for (i = 0; i < ARRAY_ELEM_NUM(g_nand_chip_desc); i++) {
 		if (g_nand_chip_desc[i].id == dev_id) {
@@ -210,24 +189,35 @@ int nand_probe(struct nand_chip *nand)
 #define NAND_CTRL_CLE		(NAND_NCE | NAND_CLE)
 #define NAND_CTRL_ALE		(NAND_NCE | NAND_ALE)
 
-
-static void __cmd_ctrl(struct nand_chip *chip, s32 cmd, u32 ctrl)
+static void nand_cmd(struct nand_chip *chip, u32 cmd, int row, int col)
 {
-	void *base = (void *)GPMC_BASE + GPMC_CS0 + (0x30 * cs);
+	chip->cmd_ctrl(chip, cmd, NAND_CTRL_CTRL_CLE);
 
-	switch (ctrl) {
-	case NAND_CTRL_CLE:
-		chip->IO_ADDR_W = base + GPMC_CS_NAND_CMD;
-		break;
-	case NAND_CTRL_ALE:
-		chip->IO_ADDR_W = base + GPMC_CS_NAND_ADR;
-		break;
-	case NAND_NCE:
-		chip->IO_ADDR_W = base + GPMC_CS_NAND_DAT;
+	if (col != -1) {
+		chip->cmd_ctrl(chip, col & 0xff, NAND_CTRL_ALE);
+
+		if (is_large_page(chip))
+			chip->cmd_ctrl(chip, (col >> 8) & 0xff, NAND_CTRL_ALE);
+	}
+
+	if (row != -1) {
+		chip->cmd_ctrl(chip, row & 0xff, NAND_CTRL_ALE);
+		chip->cmd_ctrl(chip, (row >> 8) & 0xff, NAND_CTRL_ALE);
+
+		if (chip->chip_size > (1 << 27))
+			chip->cmd_ctrl(chip, (row >> 16) & 0xff, NAND_CTRL_ALE);
+	}
+
+	switch (cmd) {
+	case NAND_CMD_READ0:
+		if (is_large_page(chip))
+			chip->cmd_ctrl(chip, NAND_CMD_READSTART, NAND_CTRL_CLE);
+
+	default:
 		break;
 	}
 
-	writeb(cmd, chip->IO_ADDR_W);
+	nand_wait_ready(chip); // fixme: not need for READID
 }
 
 /* read 8bit buswidth nands */
@@ -250,8 +240,8 @@ static void nand_read_buf16(struct nand_chip *chip, u8 *buf, int len)
 		p[i] = readw(chip->IO_ADDR_R);
 }
 
-void *nand_read_page(struct nand_chip *nand, __u32 page, void *buff)
+void *nand_read_page(struct nand_chip *chip, int page, void *buf)
 {
-	nand_command(nand, NAND_CMMD_READ0, page, 0);
-	return nand->read_buff(nand, buff, nand->write_size);
+	nand_command(chip, NAND_CMD_READ0, page, 0);
+	return nand->read_buf(chip, buf, nand->write_size);
 }
